7776c55664695660aa3a158d4feafc6da5dd03bb
Sai Rohit Voleti <sairohit33@gmail.com>
2025-03-11 10:26:13 +0530

    Fix: Set correct base path for GithubPage

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Sai-Rohit-portfolio/', // <-- ADD THIS LINE
   plugins: [react()],
+  base: '/Sai-Rohit-portfolio/',
 })
