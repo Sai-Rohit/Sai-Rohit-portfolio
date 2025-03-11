ed27c0ce37efaf6f6dd28af7b3a27a190230e859
Sai Rohit Voleti <sairohit33@gmail.com>
2025-03-11 10:33:43 +0530

    switching to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Sai-Rohit-portfolio/', // 👈 must match your repo name
   plugins: [react()],
-  base: '/Sai-Rohit-portfolio/',
 })
