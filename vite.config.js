9397e40cde56f74c51e29701a4dbf8a7ef1d2024
Sai Rohit Voleti <sairohit33@gmail.com>
2025-03-11 10:38:21 +0530

    FIx: correct base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Sai-Rohit-portfolio/', // 👈 must match your repo name
+  base: '/Sai-Rohit-portfolio/', 
   plugins: [react()],
 })
